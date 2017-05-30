4abad7f2f7bd642aeeaf4cbdf32010bc86e40ac3
AlphaMistral <Mistral@weymire.com>
2017-05-30 19:39:59 +0800

    Readme Altered
    Readme.

--- Readme.md
+++ Readme.md
@@ -1,4 +1,16 @@
 ## 3DRasEngine
 
-### Initial Commit.
+### Introduction
+* 3DRasEngine is an offline rendering engine developed based on rasterization. 
+* In order to emphasize the process of how a rendering engine works, 3DRasEngine is not optimized. 
+* Most of the ideas behind the engine could be found on https://www.scratchapixel.com
+
+### Installation and Setup
+* git clone https://github.com/AlphaMistral/3DRasEngine.git
+* cd 3DRasEngine
+* make
+* ./RasEngine
+
+### More features would be added into this rendering engine in the future. 
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,14 @@ int main ()
     renderer.SetLight ({ -10.0f, 30.0f, 30.0f }, { 0.5f, 0.0f, 0.0f, 0 }, { 0.8f, 0.8f, 0.8f, 0 }, { 0.5f, 0.5f, 0.5f, 0 });
     
     // Model (filepath, position, material)
-    Model sphere ("res/sphere", { 2.5f, 0.5f, 1.5f }, { 0.1f, 1.0f, 0.5f });
-    renderer.DrawModel (sphere, true, false);
+    //Model sphere ("res/sphere", { 2.5f, 0.5f, 1.5f }, { 0.1f, 1.0f, 0.5f });
+    //renderer.DrawModel (sphere, true, false);
     Model bunny ("res/bunny", { 0.0f, 0.0f, 0.0f }, { 0.1f, 0.8f, 0.7f });
     renderer.DrawModel (bunny, true, false);
-    Model cube ("res/cube", { -2.0f, 0.0f, 2.0f }, { 0.3f, 0.8f, 0.8f });
-    renderer.DrawModel (cube, true, false);
-    Model cubeFrame ("res/cube", { 4.0f, 1.8f, -2.2f }, { 0.5f, 0.8f, 0.8f });
-    renderer.DrawModel (cubeFrame, false, true);
+    //Model cube ("res/cube", { -2.0f, 0.0f, 2.0f }, { 0.3f, 0.8f, 0.8f });
+    //renderer.DrawModel (cube, true, false);
+    //Model cubeFrame ("res/cube", { 4.0f, 1.8f, -2.2f }, { 0.5f, 0.8f, 0.8f });
+    //renderer.DrawModel (cubeFrame, false, true);
     
     // save the frame buffer to a bmp file
     BMPManager::SaveBMP (renderer.frameBuffer, WIDTH, HEIGHT, "momo.bmp");
