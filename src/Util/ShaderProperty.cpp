7ce4c2dadf06886025ccfe5fe2e3a79883b6511a
AlphaMistral <Mistral@weymire.com>
2017-06-06 01:33:13 +0800

    MakeFile working.
    AT.

---
+++ src/Util/ShaderProperty.cpp
@@ -0,0 +1,23 @@
+//
+//  ShaderProperty.hpp
+//  3DRasEngine
+//
+//  Created by 于京平 on 2017/6/4.
+//  Copyright © 2017年 于京平. All rights reserved.
+//
+
+#ifndef ShaderProperty_hpp
+#define ShaderProperty_hpp
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <functional>
+#include <cmath>
+
+#include "Light.hpp"
+#include "BMPManager.hpp"
+#include "ObjectProperties.hpp"
+#include "ShaderProperty.hpp"
+
+#endif /* ShaderProperty_hpp */
